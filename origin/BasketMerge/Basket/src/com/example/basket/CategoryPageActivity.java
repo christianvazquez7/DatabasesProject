66978a89cc5afb36d8c81d0ed5109d6da6c6e0f1
Alberto <a.rios3991@gmail.com>
2013-09-16 14:17:13 -0400

    Alberto trabajo categories

---
+++ origin/BasketMerge/Basket/src/com/example/basket/CategoryPageActivity.java
@@ -0,0 +1,16 @@
+package com.example.basket;
+
+import android.app.Activity;
+import android.os.Bundle;
+
+public class CategoryPageActivity extends Activity{
+
+	@Override
+	protected void onCreate(Bundle savedInstanceState) {
+		// TODO Auto-generated method stub
+		super.onCreate(savedInstanceState);
+	}
+	
+	 
+
+}
