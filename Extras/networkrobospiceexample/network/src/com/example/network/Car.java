99747a45f6dbcbeef7967118abc6fdb2c0fe04ad
Pedro <pedro.colon4@upr.edu>
2013-09-18 14:17:03 -0400

    Dios.

---
+++ Extras/networkrobospiceexample/network/src/com/example/network/Car.java
@@ -0,0 +1,54 @@
+package com.example.network;
+
+public class Car {
+	private String id, make, model, year, description, price;
+
+	public String getId() {
+		return id;
+	}
+
+	public void setId(String id) {
+		this.id = id;
+	}
+
+	public String getMake() {
+		return make;
+	}
+
+	public void setMake(String make) {
+		this.make = make;
+	}
+
+	public String getModel() {
+		return model;
+	}
+
+	public void setModel(String model) {
+		this.model = model;
+	}
+
+	public String getYear() {
+		return year;
+	}
+
+	public void setYear(String year) {
+		this.year = year;
+	}
+
+	public String getDescription() {
+		return description;
+	}
+
+	public void setDescription(String description) {
+		this.description = description;
+	}
+
+	public String getPrice() {
+		return price;
+	}
+
+	public void setPrice(String price) {
+		this.price = price;
+	}
+	
+}
