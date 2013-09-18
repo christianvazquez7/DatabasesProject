cfe306d914395b8f0c04294e4099b7ecf19d1752
Pedro <pedro.colon4@upr.edu>
2013-09-18 11:58:50 -0400

    wooooooooooooooooooooooooooooooooooooooooooooooooo networking

---
+++ Extras/networkrobospiceexample/network/src/com/example/network/CarJsonSpringAndroidRequest.java
@@ -0,0 +1,19 @@
+package com.example.network;
+
+import android.util.Log;
+
+import com.octo.android.robospice.request.springandroid.SpringAndroidSpiceRequest;
+
+public class CarJsonSpringAndroidRequest extends SpringAndroidSpiceRequest<ListCars> {
+
+	public CarJsonSpringAndroidRequest() {
+		super(ListCars.class);
+	}
+
+	@Override
+	public ListCars loadDataFromNetwork() throws Exception {
+		Log.d( "request", "loading from network" );
+		return getRestTemplate().getForObject( "http://10.0.2.2:3412/ClassDemo3Srv/cars", ListCars.class );
+	}
+
+}
