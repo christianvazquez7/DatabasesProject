ed5561a8439a100a287f2d2fdaa0c285e81f19a4
christianvazquez7 <christian.vazquez7@upr.edu>
2013-09-18 15:07:43 -0400

    updated slider menu and admin pages

--- origin/BasketMerge/Basket/src/com/example/basket/AdminProductList.java
+++ origin/BasketMerge/Basket/src/com/example/basket/AdminProductList.java
@@ -1,5 +1,6 @@
 package com.example.basket;
 
+import android.content.Intent;
 import android.view.View;
 import android.widget.ListView;
 
@@ -7,6 +8,7 @@ public class AdminProductList extends ProductListFragment
 {
 	public void onListItemClick(ListView l, View v, int pos ,  long id )
 	{
-		
+		Intent productSales = new Intent(this.getActivity(),ProductSales.class);
+		this.startActivity(productSales);
 	}
 }
