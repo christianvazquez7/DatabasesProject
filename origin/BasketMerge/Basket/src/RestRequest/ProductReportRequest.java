ca5be8f3f532c7e521fb668abe21b2c4253d5320
christianvazquez7 <christian.vazquez7@upr.edu>
2013-09-29 18:06:04 -0400

    changes

---
+++ origin/BasketMerge/Basket/src/RestRequest/ProductReportRequest.java
@@ -0,0 +1,33 @@
+package RestRequest;
+
+import android.util.Log;
+
+import com.application.settings.Report;
+import com.octo.android.robospice.request.springandroid.SpringAndroidSpiceRequest;
+
+public class ProductReportRequest extends SpringAndroidSpiceRequest<Report> 
+{
+	private int day;
+	private int month;
+	private int year;
+	private String type;
+	public ProductReportRequest(int d, int m, int y,String t) 
+	{
+		super(Report.class);
+		day=d;
+		month=m;
+		year=y;
+		type=t;
+		
+	}
+
+	@Override
+	public Report loadDataFromNetwork() throws Exception 
+	{
+		
+		String url = "http://10.0.2.2:3412/Basket.js/ProductReport/";
+		url+=day+"/"+month+"/"+ year+"/"+type;
+		Log.d( "request", "loading from network" );
+		return getRestTemplate().getForObject(url, Report.class);
+	}
+}
