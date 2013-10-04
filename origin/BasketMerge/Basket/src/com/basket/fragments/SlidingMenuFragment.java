ac663ef343fdcf4ffc9752e025e7da505ec2e33f
Pedro <pedro.colon4@upr.edu>
2013-10-04 17:33:24 -0400

    Fixing bugs

--- origin/BasketMerge/Basket/src/com/basket/fragments/SlidingMenuFragment.java
+++ origin/BasketMerge/Basket/src/com/basket/fragments/SlidingMenuFragment.java
@@ -3,17 +3,6 @@ package com.basket.fragments;
 import java.util.ArrayList;
 import java.util.List;
 
-import com.basket.activities.BasketActivity;
-import com.basket.activities.CategoryPageActivity;
-import com.basket.activities.PicknickActivity;
-import com.basket.activities.ProductFragmentActivity;
-import com.basket.adapters.SectionListAdapter;
-import com.basket.general.Section;
-import com.example.basket.R;
-import com.example.basket.R.anim;
-import com.example.basket.R.id;
-import com.example.basket.R.layout;
-
 import android.app.Fragment;
 import android.content.Intent;
 import android.os.Bundle;
@@ -24,38 +13,48 @@ import android.view.animation.Animation;
 import android.view.animation.AnimationUtils;
 import android.widget.ExpandableListView;
 
+import com.basket.activities.BasketActivity;
+import com.basket.activities.CategoryPageActivity;
+import com.basket.activities.ChooseSubSubCategoryActivity;
+import com.basket.activities.PicknickActivity;
+import com.basket.activities.ProductFragmentActivity;
+import com.basket.adapters.SectionListAdapter;
+import com.basket.general.Section;
+import com.example.basket.R;
+
 public class SlidingMenuFragment extends Fragment implements ExpandableListView.OnChildClickListener {
 
 	private ExpandableListView sectionListView;
 	private Animation animSlideup, animSlidedown;
-	
+	List<Section> sectionList;
+
 	@Override
 	public View onCreateView(LayoutInflater inflater, ViewGroup container,
 			Bundle savedInstanceState) {
 
 		animSlideup = AnimationUtils.loadAnimation(getActivity().getApplicationContext(),
-	                R.anim.slide_up); 
+				R.anim.slide_up); 
 		animSlidedown = AnimationUtils.loadAnimation(getActivity().getApplicationContext(),
-	                R.anim.slide_down); 
-		
-		List<Section> sectionList = createMenu();
-		
+				R.anim.slide_down); 
+
+		sectionList = createMenu();
+
 		View view = inflater.inflate(R.layout.slidingmenu_fragment, container, false);
 		this.sectionListView = (ExpandableListView) view.findViewById(R.id.slidingmenu_view);
 		this.sectionListView.setGroupIndicator(null);
 
 		SectionListAdapter sectionListAdapter = new SectionListAdapter(this.getActivity(), sectionList);
-		
+
 		this.sectionListView.setAdapter(sectionListAdapter); 
 
-//		this.sectionListView.setOnGroupClickListener(new ExpandableListView.OnGroupClickListener() {
-//			@Override
-//			public boolean onGroupClick(ExpandableListView parent, View v, int groupPosition, long id) {
-//				//TODO
-//				parent.setAnimation(animSlidedown);
-//				return parent.expandGroup(groupPosition);
-//			}
-//		});
+		//		this.sectionListView.setOnGroupClickListener(new ExpandableListView.OnGroupClickListener() {
+		//			@Override
+		//			public boolean onGroupClick(ExpandableListView parent, View v, int groupPosition, long id) {
+		//				//TODO
+		//				parent.setAnimation(animSlidedown);
+		//				return parent.expandGroup(groupPosition);
+		//			}
+		//		});
 
 		this.sectionListView.setOnChildClickListener(this);
 
@@ -75,11 +74,11 @@ public class SlidingMenuFragment extends Fragment implements ExpandableListView.
 		oGeneralSection.addSectionItem(702, "Search Products", "monotone_arrow_right");
 		oGeneralSection.addSectionItem(703, "My Basket", "monotone_arrow_right");
 
-		
+
 		Section oCategoriesSection = new Section("Categories");
 		oCategoriesSection.setBold(true);
-		
-		
+
+
 		Section oElectronicsSection = new Section("Electronics");
 		oElectronicsSection.addSectionItem(101, "TV", "monotone_arrow_right");
 		oElectronicsSection.addSectionItem(102, "Audio", "monotone_arrow_right");
@@ -92,27 +91,27 @@ public class SlidingMenuFragment extends Fragment implements ExpandableListView.
 		oComputersSection.addSectionItem(202, "Desktops", "monotone_arrow_right");
 		oComputersSection.addSectionItem(203, "Tablets", "monotone_arrow_right");
 		oComputersSection.addSectionItem(204, "Printers", "monotone_arrow_right");
-		
+
 		Section oClothingSection = new Section("Clothing");
 		oClothingSection.addSectionItem(301, "Children", "monotone_arrow_right");
 		oClothingSection.addSectionItem(302, "Men", "monotone_arrow_right");
 		oClothingSection.addSectionItem(303, "Women", "monotone_arrow_right");
-		
+
 		Section oSportsSection = new Section("Sports");
 		oSportsSection.addSectionItem(401, "Bicycles", "monotone_arrow_right");
 		oSportsSection.addSectionItem(402, "Fishing", "monotone_arrow_right");
 		oSportsSection.addSectionItem(403, "Baseball", "monotone_arrow_right");
-		oSportsSection.addSectionItem(403, "Golf", "monotone_arrow_right");
-		oSportsSection.addSectionItem(403, "Basketball", "monotone_arrow_right");
+		oSportsSection.addSectionItem(404, "Golf", "monotone_arrow_right");
+		oSportsSection.addSectionItem(405, "Basketball", "monotone_arrow_right");
 
 		Section oBooksSection = new Section("Books");
 		oBooksSection.addSectionItem(501, "Children", "monotone_arrow_right");
 		oBooksSection.addSectionItem(502, "Fiction", "monotone_arrow_right");
 		oBooksSection.addSectionItem(503, "Technology", "monotone_arrow_right");
 		oBooksSection.addSectionItem(504, "Business", "monotone_arrow_right");
-		
+
 		Section oShoesSection = new Section("Shoes");
-	
+
 		oShoesSection.addSectionItem(601, "Children", "monotone_arrow_right");
 		oShoesSection.addSectionItem(602, "Women", "monotone_arrow_right");
 		oShoesSection.addSectionItem(603, "Men", "monotone_arrow_right");
@@ -125,8 +124,8 @@ public class SlidingMenuFragment extends Fragment implements ExpandableListView.
 		sectionList.add(oClothingSection);
 		sectionList.add(oShoesSection);
 		sectionList.add(oSportsSection);
-		
-		
+
+
 		return sectionList;
 	}
 
@@ -134,26 +133,54 @@ public class SlidingMenuFragment extends Fragment implements ExpandableListView.
 	public boolean onChildClick(ExpandableListView parent, View v,
 			int groupPosition, int childPosition, long id) {
 
-		if((int)id < 700){
-			Intent cp = new Intent(getActivity(), CategoryPageActivity.class);
-			startActivity(cp);
-		}
-		else{
+		if((int)id == 302 || (int)id == 303 || (int)id == 402){
 			//TODO
+			if((int)id == 302){
+				ArrayList<String> menClothSubCatList = new ArrayList<String>();
+				menClothSubCatList.add("Shirts");
+				menClothSubCatList.add("Pants");
+				menClothSubCatList.add("Socks");
+				Intent i = new Intent(getActivity(), ChooseSubSubCategoryActivity.class);
+				i.putStringArrayListExtra("subcategoryList", menClothSubCatList);
+				startActivity(i);
+			}
+			else if((int)id == 303){
+				ArrayList<String> womenClothSubCatList = new ArrayList<String>();
+				womenClothSubCatList.add("Shirts");
+				womenClothSubCatList.add("Pants");
+				womenClothSubCatList.add("Dresses");
+				Intent i = new Intent(getActivity(), ChooseSubSubCategoryActivity.class);
+				i.putStringArrayListExtra("subcategoryList", womenClothSubCatList);
+				startActivity(i);
+			}
+			else if((int)id == 402){
+				ArrayList<String> bicyclesSubCatList = new ArrayList<String>();
+				bicyclesSubCatList.add("Frames");
+				bicyclesSubCatList.add("Wheels");
+				bicyclesSubCatList.add("Helmets");
+				bicyclesSubCatList.add("Parts");
+				Intent i = new Intent(getActivity(), ChooseSubSubCategoryActivity.class);
+				i.putStringArrayListExtra("subcategoryList", bicyclesSubCatList);
+				startActivity(i);
+			}
+		}
+		else if((int)id < 700){
+			Intent cp = new Intent(getActivity(), CategoryPageActivity.class);
+			String catName = this.sectionList.get(groupPosition).getSectionItems()
+					.get(childPosition).getTitle();
+			cp.putExtra("categoryName", catName);
+			startActivity(cp);
 		}
 		switch ((int)id) {
 		case 701:
-			//TODO
 			Intent myPicknickIntent = new Intent(getActivity(),PicknickActivity.class);
 			startActivity(myPicknickIntent);
 			break;
 		case 702:
-			//TODO
 			Intent pl = new Intent(getActivity(),ProductFragmentActivity.class);
 			startActivity(pl);
 			break;
 		case 703:
-			//TODO
 			Intent b = new Intent(getActivity(),BasketActivity.class);
 			startActivity(b);
 			break;
